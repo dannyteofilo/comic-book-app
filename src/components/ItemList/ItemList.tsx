f1c9c02cf482f22da5f77c27ec9d2d95b2573e21
Daniel Teofilo <danyel.285309@gmail.com>
2022-03-21 01:40:03 -0600

    feat: itemList component created

---
+++ src/components/ItemList/ItemList.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import moment from "moment";
+import { itemComicInterface } from "../../interfaces/ItemComic";
+
+export const ItemList = ({ image, date_added, volume }: itemComicInterface) => {
+  return (
+    <div className="item-list">
+      <img src={image.small_url} alt={volume.name} />
+      <div className="detail">
+        <p className="title">{volume.name}</p>
+        <span className="date">{moment(date_added).format("LL")}</span>
+      </div>
+    </div>
+  );
+};
